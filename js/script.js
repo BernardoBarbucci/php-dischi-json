ee6e5d1fdeeca71d617877e6a0314b96c35a11de
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-16 16:21:04 +0100

    impostazione js con chiamata axios richiamata

--- js/script.js
+++ js/script.js
@@ -3,6 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            apiUrl: './script.php',
+            albums: [],
+        }
+    },
+    methods: {
+        getAlbums() {
+            axios.get(this.apiUrl).then((response) => {
+                this.albums = response.data;
+
+                console.log(response.data);
+            });
+        },
+
+        created() {
+            this.getAlbums();
         }
     }
 }).mount('#app')
--- php/index.php
+++ php/index.php
@@ -1,6 +1,7 @@
 <!-- front-end file -->
 
 <?php
-
-
+// $stringList = file_get_contents('./json/api.json');
+// header('Content-Type: application/json');
+// echo $stringList;
 ?>
