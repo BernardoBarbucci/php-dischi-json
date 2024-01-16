b83069ac3d0c726056a361acffce76d37b756d67
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-16 16:26:59 +0100

    minor changes per collegare i file e strutturare meglio js

--- js/script.js
+++ js/script.js
@@ -5,19 +5,17 @@ createApp({
         return {
             apiUrl: './script.php',
             albums: [],
-        }
+        };
     },
     methods: {
         getAlbums() {
             axios.get(this.apiUrl).then((response) => {
                 this.albums = response.data;
-
                 console.log(response.data);
             });
         },
-
-        created() {
-            this.getAlbums();
-        }
-    }
-}).mount('#app')
+    },
+    created() {
+        this.getAlbums();
+    },
+}).mount('#app');
